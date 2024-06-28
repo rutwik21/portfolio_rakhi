57b3e4666b901b2623243f38cf64f9cfa1b4c22d
Rutwik Kashid <98022513+rutwik21@users.noreply.github.com>
2024-06-28 17:21:56 +0530

    created portfolio site

---
+++ src/components/Footer.js
@@ -0,0 +1,68 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import {
+  AiFillGithub,
+  AiOutlineTwitter,
+  AiFillInstagram,
+} from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
+
+function Footer() {
+  let date = new Date();
+  let year = date.getFullYear();
+  return (
+    <Container fluid className="footer">
+      <Row>
+        <Col md="3" className="footer-copywright">
+          <h3>Copyright © {year}</h3>
+        </Col>
+        <Col md="3" className="footer-body">
+          <ul className="footer-icons">
+            {/* <li className="social-icons">
+              <a
+                href="https://github.com"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillGithub />
+              </a>
+            </li> */}
+            <li className="social-icons">
+              <a
+                href="https://twitter.com"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiOutlineTwitter />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.linkedin.com/in/krushnasharma05"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <FaLinkedinIn />
+              </a>
+            </li>
+            <li className="social-icons">
+              <a
+                href="https://www.instagram.com/s_rakhi01"
+                style={{ color: "white" }}
+                target="_blank" 
+                rel="noopener noreferrer"
+              >
+                <AiFillInstagram />
+              </a>
+            </li>
+          </ul>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
+
+export default Footer;
